31d70f65e140a85c195b3a5a855f8b766b9e9ebd
Octavius <yveltcl@gmail.com>
2021-01-25 19:16:33 -0500

    Began dark/light mode toggle, added contacts page

---
+++ .history/js/main_20210125185526.js
@@ -0,0 +1,16 @@
+const toggleColor = e => {
+    if (e.currentTarget.classlist.contains("light-hidden")) {
+        document.documentElement.setAttribute("color-mode", "light");
+        localStorage.setItem("color-mode", "light");
+        return;
+    }
+
+    document.documentElement.setAttribute("color-mode", "dark");
+    localStorage.setItem("color-mode", "dark");
+};
+
+const toggleColor = document.querySelectorAll(".colorbutton");
+
+toggleColor.forEach(btn => {
+    btn.addEventListener("click", toggleColor);
+});
