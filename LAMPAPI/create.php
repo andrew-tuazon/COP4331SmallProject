4acd1ffc7c44bd6880e359ad1ba933f9c49f447f
andrew <tuazonandrew@yahoo.com>
2021-01-26 21:06:34 -0500

    second commit with phps

--- LAMPAPI/create.php
+++ LAMPAPI/create.php
@@ -2,20 +2,24 @@
 
 	$inData = getRequestInfo();
 	
-	$firstName = $inData["registerFirst"];
-	$lastName = $inData["registerLast"];
-	$login = $inData["registerName"];
-	$password = $inData["registerPassword"];
-	
+	$firstName = $inData["contactFirstName"];
+	$lastName = $inData["contactLastName"];
+	$phone = $inData["phone"];
+	$email = $inData["email"];
+	$address = $inData["address"];
+	$city = $inData["city"];
+	$state = $inData["state"];
+	$zip = $inData["zip"];
+
 	$conn = new mysqli("localhost", "Team21", "COP433121Team", "COP4331");
 
 	if ($conn->connect_error) 
 	{
-		returnWithError($conn->connect_error);
-	} 
+		returnWithError( $conn->connect_error );
+	}
 	else
 	{
-		$sql = "insert into USERS (firstName, lastName, login, password) VALUES ('" . $firstName . "','". $lastName . "','" . $login . "', '" . $password . "')";
+		$sql = "insert into CONTACTS (firstName, lastName, phone, email, address, city, state, zip) VALUES ('" . $firstName . "','". $lastName . "','" . $phone . "', '" . $email . "', '" . $address . "', '" . $city . "', '" . $state . "', " . $zip . ")";
 		if($result = $conn->query($sql) != TRUE)
 		{
 			returnWithError($conn->error);
