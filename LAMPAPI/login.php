6074eb5eddf8f203f66677dde3b4683d2f6106cf
HenryP21 <henrydpinzon4321@gmail.com>
2021-02-02 19:22:29 -0500

    new commit

--- LAMPAPI/login.php
+++ LAMPAPI/login.php
@@ -5,18 +5,44 @@
     $firstName = "";
     $lastName = "";
 
-    $conn = new mysqli("localhost", "Team21", "COP433121Team", "COP4331");
+    $connection = new mysqli("localhost", "Team21", "COP433121Team", "COP4331");
 
-    if ($conn->connect_error) {
-        returnWithError($conn->connect_error)
+    if ($connection->connect_error) {
+        returnWithError($connection->connect_error);
     } else {
-        $sql = "SELECT "
+        $sql = "SELECT uid, firstName, lastName FROM Users where Login='" . $inData["login"] . "' and Password='" . $inData["password"] . "'";
+        $result = $connection->query($sql);
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $firstName = $row["firstName"];
+            $lastName = $row["lastName"];
+            $id = $row["uid"];
+
+
+            returnWithInfo($firstName, $lastName, $id);
+        } else returnWithError("No record found");
+
+        $connection->close();
     }
 
     function getRequestInfo() {
-        return json_decode(file_get_contents('php://input'), true)
+        return json_decode(file_get_contents('php://input'), true);
     }
 
+    function sendResultInfoAsJSON($obj) {
+        header('Content-type: application/json');
+        echo $obj;
+    }
 
+    function returnWithError($error) {
+        $returnValue = '{"id": ' . ', "firstName": ' . ', "lastName": ' . ', "error": ' . $error;
+        sendResultInfoAsJSON($returnValue);
+    }
+
+    function returnWithInfo($firstName, $lastName, $id) {
+        $returnValue = '{"id": ' . $id . ', "firstName": ' . $firstName . ', "lastName": ' . $lastName . '"error":""'};
+        sendResultInfoAsJSON($returnValue);
+    }
 
 ?>
--- js/login.js
+++ js/login.js
@@ -158,6 +158,44 @@ function addContact()
 
 function searchContact()
 {
+    var srch = document.getElementById("searchText").value;
+	document.getElementById("contactSearchResult").innerHTML = "";
+	
+	var contactList = "";
+	
+	var jsonPayload = '{"search" : "' + srch + '","userId" : ' + userId + '}';
+	var url = urlBase + '/Contacts.' + extension;
+	
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("contactSearchResult").innerHTML = "Contact(s) has been retrieved";
+				var jsonObject = JSON.parse( xhr.responseText );
+				
+				for( var i=0; i<jsonObject.results.length; i++ )
+				{
+					colorList += jsonObject.results[i];
+					if( i < jsonObject.results.length - 1 )
+					{
+						colorList += "<br />\r\n";
+					}
+				}
+				
+				document.getElementsByTagName("p")[0].innerHTML = contactList;
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("contactSearchResult").innerHTML = err.message;
+	}
 
 }
 
